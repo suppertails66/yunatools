e6ad237386afc2b7c2d154ffd9cf89b5627e3842
Supper <suppertails66@gmail.com>
2021-08-26 16:04:02 -0500

    wla-dx: Remove generated CMake files from version control

--- yuna/wla-dx/CMakeFiles/gens.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/gens"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/gens.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
